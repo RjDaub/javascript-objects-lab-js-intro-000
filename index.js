38705bf7d41b49b087f98cd66134aa65ba996cc3
Robert Daub <rjd2066@gmail.com>
2020-04-07 05:30:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key, value) {
+Object.assign ({}, object, {[key] : value})
+delete object.key
+return object
+}
